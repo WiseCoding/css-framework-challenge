a84b168787c1f77e733108bcc4f3915856aa28f9
Mattias Bonte <mattias_bonte@hotmail.com>
2020-07-03 01:05:33 +0200

    css purged

--- postcss.config.js
+++ postcss.config.js
@@ -1,1 +1,10 @@
-module.exports = { plugins: [require("tailwindcss"), require("autoprefixer")] };
+module.exports = {
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    require("@fullhuman/postcss-purgecss")({
+      content: ["./index.html", "./part2.html"],
+      defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+    }),
+  ],
+};
