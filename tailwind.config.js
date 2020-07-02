c34b064e6f3e07de4c13a7d597267a28de481214
Mattias Bonte <mattias_bonte@hotmail.com>
2020-07-02 17:41:22 +0200

    Long overdue, header and 2 sections finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "head-grey": "#8c9ba0",
+      },
+      flexGrow: {
+        "1": 1,
+        "2": 2,
+        "3": 3,
+        "4": 4,
+      },
+    },
   },
   variants: {},
   plugins: [],
-}
+};
