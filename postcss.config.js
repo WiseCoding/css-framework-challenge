7d8819695540138705fe6357ef61651adbca7d21
Mattias Bonte <mattias_bonte@hotmail.com>
2020-07-03 19:04:49 +0200

    purge error fixed

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,7 @@ module.exports = {
     require("tailwindcss"),
     require("autoprefixer"),
     require("@fullhuman/postcss-purgecss")({
-      content: ["./index.html", "./part2"],
+      content: ["./index.html", "./part2.html"],
       defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
     }),
   ],
