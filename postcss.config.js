343f871f28cb110a11195e8565da66b125924f86
Mattias Bonte <mattias_bonte@hotmail.com>
2020-07-03 11:45:45 +0200

    Part 2 structure done

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,3 @@
 module.exports = {
-  plugins: [
-    require("tailwindcss"),
-    require("autoprefixer"),
-    require("@fullhuman/postcss-purgecss")({
-      content: ["./index.html", "./part2.html"],
-      defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-    }),
-  ],
+  plugins: [require("tailwindcss"), require("autoprefixer")],
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     extend: {
       colors: {
         "head-grey": "#8c9ba0",
+        "body-blue": "#e6eff6",
       },
       flexGrow: {
         "1": 1,
